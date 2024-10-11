a48d5c8cba6dc88caef7935172c18bdcd22edef9
Captainnayan199 <nayanpathak233199@gmail.com>
2024-10-11 17:28:54 +0545

    First widget added

---
+++ 1. About Dialog/practice/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Simple App Bar'),
+          backgroundColor: Colors.orange,
+          centerTitle: true,
+        ),
+        body: Center(
+          child: ElevatedButton(
+              onPressed: () {
+                context;
+                (context) => const AboutDialog(
+                      applicationIcon: FlutterLogo(),
+                      applicationLegalese: "Legalese",
+                      applicationName: "Flutter App",
+                      applicationVersion: "version 1.1.1.1",
+                      children: [
+                        Text("This is a text created by Nayan Pathak"),
+                      ],
+                    );
+              },
+              child: const Text("Show dialog")),
+        ),
+      ),
+    );
+  }
+}
