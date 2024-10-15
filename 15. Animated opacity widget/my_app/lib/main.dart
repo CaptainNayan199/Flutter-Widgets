4cf8d38d946d6161ace527679073dfdfcd47b183
Captainnayan199 <nayanpathak233199@gmail.com>
2024-10-15 13:37:01 +0545

    Animated padding widget added

---
+++ 15. Animated opacity widget/my_app/lib/main.dart
@@ -0,0 +1,58 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, unnecessary_brace_in_string_interps, avoid_unnecessary_containers, sized_box_for_whitespace
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  double paddValue = 0;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Animated Opacity"),
+          backgroundColor: Colors.green,
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  setState(
+                    () {
+                      paddValue = paddValue == 0 ? 100 : 0;
+                    },
+                  );
+                },
+                child: Text("Change Padding"),
+              ),
+              Text("Padding : ${paddValue}"),
+              AnimatedPadding(
+                padding: EdgeInsets.all(paddValue),
+                duration: Duration(seconds: 1),
+                curve: Curves.easeInOut,
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height / 4,
+                  color: Colors.orange,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
