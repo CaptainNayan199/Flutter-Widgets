07435567e007fa134d92b0cd7c46934143865f70
Captainnayan199 <nayanpathak233199@gmail.com>
2024-10-18 14:29:50 +0545

    Chip widget added

---
+++ 35. Chip Widget/my_app/lib/main.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('AppBar'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Builder(builder: (context) {
+            return Chip(
+              label: Text("Flutter chip"),
+              backgroundColor: Colors.white,
+              // color: WidgetStateProperty.all(Colors.black),
+              onDeleted: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    duration: Duration(seconds: 1),
+                    content: Text("Chip deleted"),
+                  ),
+                );
+              },
+            );
+          }),
+        ),
+      ),
+    );
+  }
+}
