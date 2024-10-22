2ddf5659b2e6218c3adcb5928be00d4d7a1edafc
Captainnayan199 <nayanpathak233199@gmail.com>
2024-10-22 20:15:57 +0545

    Clip Path widget added

---
+++ 40. Clip Path Widget/my_app/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Clip Path'),
+          centerTitle: true,
+        ),
+        body: ClipPath(
+          clipper: MyClipper(),
+          child: Container(
+            width: double.infinity,
+            height: 300,
+            color: Colors.orangeAccent,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    return Path()
+      ..lineTo(0, size.height)
+      ..quadraticBezierTo(
+        size.width / 4,
+        size.height - 50,
+        size.width / 2,
+        size.height - 25,
+      )
+      ..quadraticBezierTo(
+        3 / 4 * size.width,
+        size.height,
+        size.width,
+        size.height - 20,
+      )
+      ..lineTo(size.width, 0);
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
